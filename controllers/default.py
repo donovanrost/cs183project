32cc5428082921c3be5daa5390f41d7dc0b5f1bc
Luca de Alfaro <luca@dealfaro.org>
2018-04-17 16:14:48 -0700

    checkpoint

--- controllers/default.py
+++ controllers/default.py
@@ -25,7 +25,7 @@ def index():
 
 
 def no_swearing(form):
-    if 'shit' in form.vars.memo:
+    if 'fool' in form.vars.memo:
         form.errors.memo = T('No swearing please')
 
 def add():
@@ -38,6 +38,35 @@ def add():
         session.flash = T('Please correct the info')
     return dict(form=form)
 
+@auth.requires_login()
+def edit():
+    """
+    - "/edit/3" it offers a form to edit a checklist.
+    'edit' is the controller (this function)
+    '3' is request.args[0]
+    """
+    if request.args(0) is None:
+        # We send you back to the general index.
+        redirect(URL('default', 'index'))
+    else:
+        q = ((db.checklist.user_email == auth.user.email) &
+             (db.checklist.id == request.args(0)))
+        # I fish out the first element of the query, if there is one, otherwise None.
+        cl = db(q).select().first()
+        if cl is None:
+            session.flash = T('Not Authorized')
+            redirect(URL('default', 'index'))
+        # Always write invariants in your code.
+        # Here, the invariant is that the checklist is known to exist.
+        # Is this an edit form?
+        form = SQLFORM(db.checklist, record=cl, deletable=False)
+        if form.process(onvalidation=no_swearing).accepted:
+            # At this point, the record has already been edited.
+            session.flash = T('Checklist edited.')
+            redirect(URL('default', 'index'))
+        elif form.errors:
+            session.flash = T('Please enter correct values.')
+    return dict(form=form)
 
 def user():
     """
