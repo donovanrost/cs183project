eff3318051c9e228db40f56047400baf23d4ec43
Leon Pham <lpham10@ucsc.edu>
2018-06-05 18:20:54 -0700

    added listing table

--- models/tables.py
+++ models/tables.py
@@ -13,14 +13,9 @@ def get_user_email():
     return auth.user.email if auth.user else None
 
 db.define_table('listings',
+                Field('property_id', 'reference property'),
                 Field('user_email', default=get_user_email()),
-                Field('address', default="411 Porter-Kresge Rd, Santa Cruz CA, 95064"),
-                Field('bedrooms', default=1),
-                Field('bathrooms', default=1),
-                Field('listed_by', 'reference auth_user', default=auth.user_id),
-                Field('image'),
                 Field('listed_on', 'datetime', default=datetime.datetime.now),
-                Field('property_type')
                 )
 
 
